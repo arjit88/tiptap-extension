177b63ca480dbbf6e0f5d23567158d5195b05002
arjit88 <arjitanand88@gmail.com>
2024-12-27 23:30:40 +0530

    Add necessary comments to understand the code better.

--- src/components/Tiptap.jsx
+++ src/components/Tiptap.jsx
@@ -2,20 +2,43 @@ import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Underline from "@tiptap/extension-underline";
 
-const extensions = [StarterKit, Underline];
+// All Necessary Extentions.
+const extensions = [StarterKit, Underline, Link];
 const content = ``;
 
+// Using useEditor hook for using the extentions.
 const Tiptap = () => {
   const editor = useEditor({
     extensions,
     content,
   });
 
+  // Component renders only when the editor instance is properly initialized.
   if (!editor) return null;
+
+  {
+    /* Function for adding & removing Links */
+  }
+  const addLink = () => {
+    const url = prompt("Enter the URL");
+    if (url) {
+      editor
+        .chain()
+        .focus()
+        .extendMarkRange("link")
+        .setLink({ href: url })
+        .run();
+    }
+  };
+
+  const removeLink = () => {
+    editor.chain().focus().unsetLink().run();
+  };
 
   return (
     <div>
       <div>
+        {/* Bold Font */}
         <button
           onClick={() => editor.chain().focus().toggleBold().run()}
           disabled={!editor.can().chain().focus().toggleBold().run()}
@@ -24,6 +47,7 @@ const Tiptap = () => {
           <strong>B</strong>
         </button>
 
+        {/* Italic Font */}
         <button
           onClick={() => editor.chain().focus().toggleItalic().run()}
           disabled={!editor.can().chain().focus().toggleItalic().run()}
@@ -32,6 +56,7 @@ const Tiptap = () => {
           Italic
         </button>
 
+        {/* Underline button */}
         <button
           onClick={() => editor.chain().focus().toggleUnderline().run()}
           disabled={!editor.can().chain().focus().toggleUnderline().run()}
@@ -40,6 +65,7 @@ const Tiptap = () => {
           Underline
         </button>
 
+        {/* Strike button */}
         <button
           onClick={() => editor.chain().focus().toggleStrike().run()}
           disabled={!editor.can().chain().focus().toggleStrike().run()}
@@ -48,6 +74,7 @@ const Tiptap = () => {
           Strike
         </button>
 
+        {/* Code button */}
         <button
           onClick={() => editor.chain().focus().toggleCode().run()}
           disabled={!editor.can().chain().focus().toggleCode().run()}
@@ -56,14 +83,17 @@ const Tiptap = () => {
           Code
         </button>
 
+        {/* Clear Marks button */}
         <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
           Clear marks
         </button>
 
+        {/*Cn button */}
         <button onClick={() => editor.chain().focus().clearNodes().run()}>
           Clear nodes
         </button>
 
+        {/* Paragraph button */}
         <button
           onClick={() => editor.chain().focus().setParagraph().run()}
           className={editor.isActive("paragraph") ? "is-active" : ""}
@@ -71,6 +101,7 @@ const Tiptap = () => {
           Paragraph
         </button>
 
+        {/* H1 button */}
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 1 }).run()
@@ -82,6 +113,7 @@ const Tiptap = () => {
           H1
         </button>
 
+        {/* H2 button */}
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 2 }).run()
@@ -93,6 +125,7 @@ const Tiptap = () => {
           H2
         </button>
 
+        {/* H3 button */}
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 3 }).run()
@@ -104,6 +137,7 @@ const Tiptap = () => {
           H3
         </button>
 
+        {/* H4 button */}
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 4 }).run()
@@ -115,6 +149,7 @@ const Tiptap = () => {
           H4
         </button>
 
+        {/* H5 button */}
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 5 }).run()
@@ -126,6 +161,7 @@ const Tiptap = () => {
           H5
         </button>
 
+        {/* H6 button */}
         <button
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 6 }).run()
@@ -137,6 +173,7 @@ const Tiptap = () => {
           H6
         </button>
 
+        {/* Ul button */}
         <button
           onClick={() => editor.chain().focus().toggleBulletList().run()}
           className={editor.isActive("bulletList") ? "is-active" : ""}
@@ -144,6 +181,7 @@ const Tiptap = () => {
           Unordered List
         </button>
 
+        {/* Ol button */}
         <button
           onClick={() => editor.chain().focus().toggleOrderedList().run()}
           className={editor.isActive("orderedList") ? "is-active" : ""}
@@ -151,6 +189,7 @@ const Tiptap = () => {
           Ordered list
         </button>
 
+        {/* Cb button */}
         <button
           onClick={() => editor.chain().focus().toggleCodeBlock().run()}
           className={editor.isActive("codeBlock") ? "is-active" : ""}
@@ -158,6 +197,7 @@ const Tiptap = () => {
           Code block
         </button>
 
+        {/* Bq button */}
         <button
           onClick={() => editor.chain().focus().toggleBlockquote().run()}
           className={editor.isActive("blockquote") ? "is-active" : ""}
@@ -165,16 +205,19 @@ const Tiptap = () => {
           Blockquote
         </button>
 
+        {/* Hr button */}
         <button
           onClick={() => editor.chain().focus().setHorizontalRule().run()}
         >
           Horizontal rule
         </button>
 
+        {/* Hard Break button */}
         <button onClick={() => editor.chain().focus().setHardBreak().run()}>
           Hard break
         </button>
 
+        {/* Undo button */}
         <button
           onClick={() => editor.chain().focus().undo().run()}
           disabled={!editor.can().chain().focus().undo().run()}
@@ -182,14 +225,20 @@ const Tiptap = () => {
           Undo
         </button>
 
+        {/* Redo button */}
         <button
           onClick={() => editor.chain().focus().redo().run()}
           disabled={!editor.can().chain().focus().redo().run()}
         >
           Redo
         </button>
+
+        {/* Link buttons */}
+        <button onClick={addLink}>Add Link</button>
+        <button onClick={removeLink}>Remove Link</button>
       </div>
 
+      {/* Enabeling the editor */}
       <div>
         <EditorContent editor={editor} />
       </div>
